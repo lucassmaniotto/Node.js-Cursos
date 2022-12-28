fe80c3738d552fb04c71e1c12eabc1349f067e21
Lucas Smaniotto <lucas91755@gmail.com>
2022-12-28 14:15:32 -0300

    Manipulando DOM com o consumo de API local

---
+++ Front-end/AluraPlay/js/showVideos.js
@@ -0,0 +1,28 @@
+import { apiConection } from "./apiConetion";
+
+const list = document.querySelector('[data-list]');
+
+function buildCard(title, description, url, image) {
+    const video = document.createElement('li');
+    video.classList = "videos__item";
+    video.innerHTML = `
+    <iframe width="100%" height="72%" src="${url}"
+        title="${title}" frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen>
+    </iframe>
+    <div class="descricao-video">
+        <img src="${image}" alt="logo canal alura">
+        <h3>${title}</h3>
+        <p>${description}</p>
+    </div>
+    `
+    return video;
+}
+
+async function showVideos() {
+    const videos = await apiConection.videosList();
+    videos.forEach(element => list.appendChild(buildCard(element.titulo, element.descricao, element.url, element.imagem)));
+}
+
+showVideos();
