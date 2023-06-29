3e7dfd6f8c29e342ac8c1acac5c8935bc9d8f00c
Lucas Smaniotto <lucas91755@gmail.com>
2023-06-29 16:55:07 -0300

    feat: criando middleware de validação de token JWT

--- Udemy/JavaScript - Básico ao Avançado/Seção 13 - API REST/src/routes/user.js
+++ Udemy/JavaScript - Básico ao Avançado/Seção 13 - API REST/src/routes/user.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import userController from '../controllers/User';
+import loginRequired from '../middlewares/loginRequired';
 
 const routes = new Router();
 
 routes.post('/', userController.create);
-routes.get('/', userController.index);
+routes.get('/', loginRequired, userController.index);
 routes.get('/:id', userController.show);
 routes.put('/:id', userController.update);
 routes.delete('/:id', userController.delete);
