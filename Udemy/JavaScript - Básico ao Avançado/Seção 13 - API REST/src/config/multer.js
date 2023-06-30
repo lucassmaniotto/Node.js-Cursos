13a56b6d652b955d3adbbb9891db55540e6e6761
Lucas Smaniotto <lucas91755@gmail.com>
2023-06-30 15:02:55 -0300

    refactor: tratamento de arquivos

--- Udemy/JavaScript - Básico ao Avançado/Seção 13 - API REST/src/config/multer.js
+++ Udemy/JavaScript - Básico ao Avançado/Seção 13 - API REST/src/config/multer.js
@@ -2,6 +2,12 @@ import multer from 'multer';
 import { extname, resolve } from 'path';
 
 export default {
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype !== 'image/png' && file.mimetype !== 'image/jpeg') {
+      return cb(new multer.MulterError('File must be PNG or JPG.'));
+    }
+    return cb(null, true);
+  },
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
       cb(null, resolve(__dirname, '..', '..', 'uploads'));
--- Udemy/JavaScript - Básico ao Avançado/Seção 13 - API REST/src/controllers/Image.js
+++ Udemy/JavaScript - Básico ao Avançado/Seção 13 - API REST/src/controllers/Image.js
@@ -1,6 +1,15 @@
+import multer from 'multer';
+import multerConfig from '../config/multer';
+
+const upload = multer(multerConfig).single('file');
 class ImageController {
   async create(req, res) {
-    return res.json(req.file);
+    return upload(req, res, (error) => {
+      if (error) {
+        return res.status(400).json({ errors: [error.code] });
+      }
+      return res.json(req.file);
+    });
   }
 }
 
